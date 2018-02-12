4049d70d978da839a67bcd7068045486587f9c4a
Claudiu Ceia <claudiu.ceia+1@gmail.com>
2018-02-12 15:29:42 +0200

    feat(header) Customize header layout

--- site/web/app/themes/smart-city/resources/views/partials/content-page.blade.php
+++ site/web/app/themes/smart-city/resources/views/partials/content-page.blade.php
@@ -1,2 +1,4 @@
-@php(the_content())
-{!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
+<div class="container">
+  @php(the_content())
+  {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
+</div>
