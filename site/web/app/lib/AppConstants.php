d82fcb9a072f350ce3ba3a1ff694c3e6722b2501
Claudiu Ceia <claudiu.ceia+1@gmail.com>
2018-02-12 02:01:53 +0200

    feat(custom) Custom post types, custom fields
    Custom post types:
    - Proiecte, companii, etichete proiecte, statistici
    
    Custom field types:
    - Proiecte, companii, etichete proiecte, statistici

---
+++ site/web/app/lib/AppConstants.php
@@ -0,0 +1,8 @@
+<?php
+
+final class AppConstants {
+  const POST_TYPE_PROJECT = 'project';
+  const POST_TYPE_COMPANY = 'company';
+  const POST_TYPE_PROJECT_LABEL = 'project_label';
+  const POST_TYPE_STATS = 'stats';
+}
