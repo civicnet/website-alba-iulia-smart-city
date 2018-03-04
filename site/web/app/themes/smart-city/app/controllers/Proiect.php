d0689e348f48880c914c4ba172beba9acd0c8cca
Claudiu Ceia <claudiu.ceia+1@gmail.com>
2018-03-04 17:49:47 +0200

    feat(project) Fill in missing details on project page
    - header 90% finished
    - timeline 90% finished
    - partners / suppliers, done
    - content 70% finished
    - stats 90% finished

--- site/web/app/themes/smart-city/app/controllers/Proiect.php
+++ site/web/app/themes/smart-city/app/controllers/Proiect.php
@@ -27,10 +27,60 @@ class Proiect extends Controller {
       return $image[0];
     }
 
-    return null; 
+    return null;
   }
 
   public static function etichete(): array {
     return get_field('etichete');
+  }
+
+  public static function furnizori(): array {
+    $furnizori = get_field('furnizori');
+    return self::parseCompanies($furnizori);
+  }
+
+  public static function parteneri(): array {
+    $parteneri = get_field('parteneri');
+    return self::parseCompanies($parteneri);
+  }
+
+  public static function descriere(): string {
+    return get_field('descriere');
+  }
+
+  public static function specificatii(): ?string {
+    return get_field('specificatii');
+  }
+
+  public static function protocol(): string {
+    return 'Coming soon';
+  }
+
+  public static function media(): string {
+    return 'Coming soon';
+  }
+
+  public static function noutati(): ?string {
+    return get_field('noutati');
+  }
+
+  public static function altceva(): ?string {
+    return get_field('altceva');
+  }
+
+  protected static function parseCompanies(array $companies): array {
+      $ret = array();
+      foreach ($companies as $company) {
+        $ret[] = array(
+          'name' => get_field('nume', $company->ID),
+          'logo' => get_field('logo', $company->ID),
+          'locatie' => get_field('locatie_companie', $company->ID),
+          'email' => get_field('adresa_email', $company->ID),
+          'facebook' => get_field('pagina_facebook', $company->ID),
+          'www' => get_field('adresa_www', $company->ID)
+        );
+      }
+
+      return $ret;
   }
 }
