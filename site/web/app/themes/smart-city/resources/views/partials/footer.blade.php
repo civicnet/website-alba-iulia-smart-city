3e302757a8bdc32b1c76e033037c310b5430ce4b
Claudiu Ceia <claudiu.ceia+1@gmail.com>
2018-03-04 23:18:35 +0200

    feat(footer) Add footer, update wp config

--- site/web/app/themes/smart-city/resources/views/partials/footer.blade.php
+++ site/web/app/themes/smart-city/resources/views/partials/footer.blade.php
@@ -1,5 +1,26 @@
 <footer class="content-info">
   <div class="container">
-    @php(dynamic_sidebar('sidebar-footer'))
+    <div class="row links">
+      <div class="col-3">
+        @php(dynamic_sidebar('sidebar-footer'))
+      </div>
+      <div class="col-3">
+        Legaturi utile
+      </div>
+      <div class="col-3">
+        Adresa
+      </div>
+      <div class="col-3">
+        Social
+      </div>
+    </div>
+    <div class="row logos">
+      <div class="alba-logo col-6">
+        Alba Iulia
+      </div>
+      <div class="civic-tech-logo col-6">
+        CivicTech
+      </div>
+    </div>
   </div>
 </footer>
