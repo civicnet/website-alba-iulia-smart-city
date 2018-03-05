c624adb8332a4e5f8df02a766c2db918e9376a82
Claudiu Ceia <claudiu.ceia+1@gmail.com>
2018-03-05 21:36:34 +0200

    fix(homepage) Add header

---
+++ site/web/app/themes/smart-city/resources/views/partials/acasa-page-header.blade.php
@@ -0,0 +1,7 @@
+<div class="page-header">
+  <div class="container">
+    <div class="motto">
+      <h1 class=title><span>{{ $title1 }}</span><br/><span>{{ $title2 }}</span></h1>
+    </div>
+  </div>
+</div>
