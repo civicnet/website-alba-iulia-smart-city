ea58a24be36a8d2054e03add1a0ffd648cb889c0
Claudiu Ceia <claudiu.ceia+1@gmail.com>
2018-03-06 02:00:07 +0200

    feat(home) get started with the "magic-star"

--- site/web/app/themes/smart-city/resources/assets/scripts/routes/home.js
+++ site/web/app/themes/smart-city/resources/assets/scripts/routes/home.js
@@ -1,6 +1,43 @@
+//import Snap from 'snapsvg';
+import anime from 'animejs';
+
 export default {
   init() {
     // JavaScript to be fired on the home page
+    $('#svg2').click(() => {
+    let timeline = anime.timeline();
+
+    timeline
+      .add({
+        targets: '#svg2 path',
+        strokeDashoffset: [anime.setDashoffset, 0],
+        easing: 'easeInQuad',
+        duration: 1000,
+        delay: function(el, i) {
+          return i * 50
+        },
+      })
+      .add({
+        targets: '#svg2 path',
+        fillOpacity: 1,
+        strokeWidth: 0,
+        easing: 'easeInOutQuad',
+      })
+      .add({
+        targets: '#svg2',
+        easing: 'easeInOutQuad',
+      })
+      .add({
+        targets: '#svg2 path',
+        fillOpacity: 0.2,
+        easing: 'easeInOutQuad',
+      });
+
+      timeline.finished.then(() => {
+        $('#svg2').css({'filter': 'grayscale(100%)'});
+        $('#svg2 path').css({'stroke': 'none'});
+      });
+    });
   },
   finalize() {
     // JavaScript to be fired on the home page, after the init JS
