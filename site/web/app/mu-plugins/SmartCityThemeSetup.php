79ae83f99db26a7187bea053139272e989a06fd5
Claudiu Ceia <claudiu.ceia+1@gmail.com>
2018-03-07 03:32:00 +0200

    fix(home) Add projects section, about section

--- site/web/app/mu-plugins/SmartCityThemeSetup.php
+++ site/web/app/mu-plugins/SmartCityThemeSetup.php
@@ -755,6 +755,21 @@ final class SmartCityThemeSetup {
     			'choices' => array(
     			),
     		),
+        array(
+    			'key' => 'field_5a9f0d3899d11',
+    			'label' => 'Culoare',
+    			'name' => 'culoare',
+    			'type' => 'color_picker',
+    			'instructions' => '',
+    			'required' => 1,
+    			'conditional_logic' => 0,
+    			'wrapper' => array(
+    				'width' => '',
+    				'class' => '',
+    				'id' => '',
+    			),
+    			'default_value' => '#996633',
+    		),
       ),
       'location' => array(
         array(
--- site/web/app/themes/smart-city/resources/assets/scripts/routes/home.js
+++ site/web/app/themes/smart-city/resources/assets/scripts/routes/home.js
@@ -5,41 +5,67 @@ export default {
   init() {
     // JavaScript to be fired on the home page
     $('#svg2').click(() => {
-    let timeline = anime.timeline();
+      let timeline = anime.timeline();
 
-    timeline
-      .add({
-        targets: '#svg2 path',
-        strokeDashoffset: [anime.setDashoffset, 0],
-        easing: 'easeInQuad',
-        duration: 1000,
-        delay: function(el, i) {
-          return i * 50
-        },
-      })
-      .add({
-        targets: '#svg2 path',
-        fillOpacity: 1,
-        strokeWidth: 0,
-        easing: 'easeInOutQuad',
-      })
-      .add({
-        targets: '#svg2',
-        easing: 'easeInOutQuad',
-      })
-      .add({
-        targets: '#svg2 path',
-        fillOpacity: 0.2,
-        easing: 'easeInOutQuad',
-      });
+      timeline
+        .add({
+          targets: '#svg2 path',
+          strokeDashoffset: [anime.setDashoffset, 0],
+          easing: 'easeInQuad',
+          duration: 1000,
+          delay: function(el, i) {
+            return i * 50
+          },
+        })
+        .add({
+          targets: '#svg2 path',
+          fillOpacity: 1,
+          strokeWidth: 0,
+          easing: 'easeInOutQuad',
+        })
+        .add({
+          targets: '#svg2',
+          easing: 'easeInOutQuad',
+        })
+        .add({
+          targets: '#svg2 path',
+          fillOpacity: 0.2,
+          easing: 'easeInOutQuad',
+        });
 
-      timeline.finished.then(() => {
-        $('#svg2').css({'filter': 'grayscale(100%)'});
-        $('#svg2 path').css({'stroke': 'none'});
+        timeline.finished.then(() => {
+          $('#svg2').css({'filter': 'grayscale(100%)'});
+          $('#svg2 path').css({'stroke': 'none'});
+        });
       });
-    });
   },
   finalize() {
     // JavaScript to be fired on the home page, after the init JS
+    $('.project-box .body').each((idx, elem) => {
+      let parent = $(elem).parent();
+      let color = $(elem).attr('data-tint');
+      let className = 'dynamic-tint-' + color.slice(1);
+
+      parent.addClass(className);
+      parent.append(`
+        <style>
+          .${className} .body::before {
+            background-color: ${color};
+          }
+
+          .${className} .footer .label {
+            color: ${color};
+          }
+
+          .${className} .footer .label .icon {
+            color: ${color};
+          }
+
+          .${className} .body .label .icon {
+            color: ${color};
+          }
+        </style>
+      `);
+    });
   },
 };
