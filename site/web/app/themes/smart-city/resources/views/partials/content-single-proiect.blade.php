7db6499fc21a7c218ab617aee20010e911e03411
Claudiu Ceia <claudiu.ceia+1@gmail.com>
2018-03-11 22:49:19 +0200

    fix(project) allow adding comments

--- site/web/app/themes/smart-city/resources/views/partials/content-single-proiect.blade.php
+++ site/web/app/themes/smart-city/resources/views/partials/content-single-proiect.blade.php
@@ -137,5 +137,4 @@
   <footer>
     {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
   </footer>
-  @php(comments_template('/partials/comments.blade.php'))
 </article>
