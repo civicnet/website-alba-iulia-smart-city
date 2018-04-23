124f7617312c900c826e63521b7e2e208940bf23
Claudiu Ceia <claudiu.ceia+1@gmail.com>
2018-04-24 02:10:11 +0300

    feat(blog) blog page template

--- site/web/app/themes/smart-city/resources/views/index.blade.php
+++ site/web/app/themes/smart-city/resources/views/index.blade.php
@@ -3,16 +3,19 @@
 @section('content')
   @include('partials.page-header', ['title' => pll__('Blog')])
 
-  @if (!have_posts())
-    <div class="alert alert-warning">
-      {{ __('Sorry, no results were found.', 'sage') }}
+  <div class="container">
+    <div class="row">
+      <div class="filters">
+
+      </div>
     </div>
-    {!! get_search_form(false) !!}
-  @endif
-
-  @while (have_posts()) @php(the_post())
-    @include('partials.content-'.get_post_type())
-  @endwhile
-
+    <div class="row no-gutters posts">
+      @while (have_posts()) @php(the_post())
+        <div class="col-md-3 col-sm-12 article-wrap">
+          @include('partials.content-'.get_post_type())
+        </div>
+      @endwhile
+    </div>
+  </div>
   {!! get_the_posts_navigation() !!}
 @endsection
--- site/web/app/themes/smart-city/resources/views/partials/content.blade.php
+++ site/web/app/themes/smart-city/resources/views/partials/content.blade.php
@@ -1,9 +1,34 @@
-<article @php(post_class())>
-  <header>
-    <h2 class="entry-title"><a href="{{ get_permalink() }}">{{ get_the_title() }}</a></h2>
-    @include('partials/entry-meta')
-  </header>
-  <div class="entry-summary">
-    @php(the_excerpt())
-  </div>
+<article @php(post_class('post-listing'))>
+  <a href="{{ get_permalink() }}">
+    <header>
+      <img src="{{ Index::featuredImage() }}" class="featured" />
+    </header>
+    <div class="category">
+      {{ Index::category() }}
+    </div>
+    <h2 class="entry-title">
+        {{ get_the_title() }}
+    </h2>
+
+    <div class="meta container">
+      <div class="row no-gutters">
+        <div class="col-5">
+          <i class="fas fa-pencil-alt"></i>
+          <span class="author">{{ get_the_author() }}</span>
+        </div>
+        <div class="col-5">
+          <i class="fas fa-calendar-alt"></i>
+          <time class="date">{{ get_post_time('d.M Y', true) }}</time>
+        </div>
+        <div class="col-2">
+          <i class="fas fa-comments"></i>
+          <span
+            class="comments disqus-comment-count"
+            data-disqus-url="{{ get_permalink() }}">
+            0
+          </span>
+        </div>
+      </div>
+    </div>
+  </a>
 </article>
