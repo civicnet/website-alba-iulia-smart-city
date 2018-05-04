7ba1f035f08e0379c91b8c5a38d06b101b64d58e
Claudiu Ceia <claudiu.ceia+1@gmail.com>
2018-05-04 06:30:46 +0300

    feat(contact) add contact page

--- site/web/app/themes/smart-city/resources/views/partials/contact-content-page.blade.php
+++ site/web/app/themes/smart-city/resources/views/partials/contact-content-page.blade.php
@@ -1,4 +1,281 @@
-<div class="container-fluid contact-fluid">
-
-  {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
+<div class="container contact">
+  <h2>{{ pll__('Vrei să ne transmiți un mesaj?') }}</h2>
+  <div class="row">
+    <div class="col-md-6 col-sm-12">
+      <div class="address">
+        {{ pll__('Adresă') }}: Calea Moților, nr.1 A, Alba Iulia <br/>
+        {{ pll__('Telefon') }}: 021.312.25.47<br/>
+        {{ pll__('Centrala') }}: 021.303.70.80<br/>
+        <br/>
+        {{ pll__('Fax') }}: 021.313.71.55/021.264.86.46<br/>
+        {{ pll__('E-mail') }}: {{ pll__('salut@albaiuliasmartcity.ro') }}<br/>
+      </div>
+    </div>
+    <div class="col-md-6 col-sm-12">
+      <div class="contact-form">
+        {!! do_shortcode('[pirate_forms]') !!}
+      </div>
+    </div>
+  </div>
 </div>
+
+<h2>{{ pll__('Unde ne găsești') }}</h2>
+<div class="container-fluid">
+  <div class="row">
+    <div class="map-container" id="map"></div>
+  </div>
+</div>
+
+<script>
+  function initMap() {
+    var marker_location = { lat: 46.0732391, lng: 23.5775401 };
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: marker_location,
+      zoom: 17,
+      styles: [
+        {
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#1d2c4d"
+            }
+          ]
+        },
+        {
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#8ec3b9"
+            }
+          ]
+        },
+        {
+          "elementType": "labels.text.stroke",
+          "stylers": [
+            {
+              "color": "#1a3646"
+            }
+          ]
+        },
+        {
+          "featureType": "administrative.country",
+          "elementType": "geometry.stroke",
+          "stylers": [
+            {
+              "color": "#4b6878"
+            }
+          ]
+        },
+        {
+          "featureType": "administrative.land_parcel",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#64779e"
+            }
+          ]
+        },
+        {
+          "featureType": "administrative.province",
+          "elementType": "geometry.stroke",
+          "stylers": [
+            {
+              "color": "#4b6878"
+            }
+          ]
+        },
+        {
+          "featureType": "landscape.man_made",
+          "elementType": "geometry.stroke",
+          "stylers": [
+            {
+              "color": "#334e87"
+            }
+          ]
+        },
+        {
+          "featureType": "landscape.natural",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#023e58"
+            }
+          ]
+        },
+        {
+          "featureType": "poi",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#283d6a"
+            }
+          ]
+        },
+        {
+          "featureType": "poi",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#6f9ba5"
+            }
+          ]
+        },
+        {
+          "featureType": "poi",
+          "elementType": "labels.text.stroke",
+          "stylers": [
+            {
+              "color": "#1d2c4d"
+            }
+          ]
+        },
+        {
+          "featureType": "poi.park",
+          "elementType": "geometry.fill",
+          "stylers": [
+            {
+              "color": "#023e58"
+            }
+          ]
+        },
+        {
+          "featureType": "poi.park",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#3C7680"
+            }
+          ]
+        },
+        {
+          "featureType": "road",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#304a7d"
+            }
+          ]
+        },
+        {
+          "featureType": "road",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#98a5be"
+            }
+          ]
+        },
+        {
+          "featureType": "road",
+          "elementType": "labels.text.stroke",
+          "stylers": [
+            {
+              "color": "#1d2c4d"
+            }
+          ]
+        },
+        {
+          "featureType": "road.highway",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#2c6675"
+            }
+          ]
+        },
+        {
+          "featureType": "road.highway",
+          "elementType": "geometry.stroke",
+          "stylers": [
+            {
+              "color": "#255763"
+            }
+          ]
+        },
+        {
+          "featureType": "road.highway",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#b0d5ce"
+            }
+          ]
+        },
+        {
+          "featureType": "road.highway",
+          "elementType": "labels.text.stroke",
+          "stylers": [
+            {
+              "color": "#023e58"
+            }
+          ]
+        },
+        {
+          "featureType": "transit",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#98a5be"
+            }
+          ]
+        },
+        {
+          "featureType": "transit",
+          "elementType": "labels.text.stroke",
+          "stylers": [
+            {
+              "color": "#1d2c4d"
+            }
+          ]
+        },
+        {
+          "featureType": "transit.line",
+          "elementType": "geometry.fill",
+          "stylers": [
+            {
+              "color": "#283d6a"
+            }
+          ]
+        },
+        {
+          "featureType": "transit.station",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#3a4762"
+            }
+          ]
+        },
+        {
+          "featureType": "water",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#0e1626"
+            }
+          ]
+        },
+        {
+          "featureType": "water",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#4e6d70"
+            }
+          ]
+        }
+      ]
+    });
+    var marker = new google.maps.Marker({
+      position: marker_location,
+      map: map
+    });
+  }
+</script>
+
+<script
+  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBub8d63h5-TPzR-xN0Qkzw1xdxXygGUjg
+&callback=initMap"
+  async
+  defer>
+</script>
--- site/web/app/themes/smart-city/resources/views/partials/contact-page-header.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="page-header">
-  <div class="container">
-    <h1 class=title>{{ $title }}</h1>
-  </div>
-</div>
