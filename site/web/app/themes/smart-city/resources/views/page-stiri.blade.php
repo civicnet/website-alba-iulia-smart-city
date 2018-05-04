bb252843976586ed9e5068f67216bdc18e8a1b5f
Claudiu Ceia <claudiu.ceia+1@gmail.com>
2018-05-04 05:16:20 +0300

    feat(news) add news templates

---
+++ site/web/app/themes/smart-city/resources/views/page-stiri.blade.php
@@ -0,0 +1,132 @@
+@extends('layouts.app')
+
+@section('content')
+  @include('partials.page-header', ['title' => pll__('Știri')])
+
+  <div class="container-fluid blog-container">
+    <div class="container">
+      <div class="row">
+        <div class="col filters">
+
+          <div id="search-box">
+            <div id="algolia-search-box"></div>
+
+            <script type="text/html" id="powered-by-template">
+              <div class="powered-by">
+                <span class="logo">
+                  <i class="fab fa-algolia"></i>
+                </span>
+                <span class="tagline">
+                  by algolia
+                </span>
+              </div>
+            </script>
+          </div>
+
+        </div>
+      </div>
+
+      <div id="algolia-hits"></div>
+
+      <script type="text/html" id="hit-template">
+        <article class="post-listing">
+          <a href="@{{permalink}}">
+            <header>
+              <img src="@{{image}}" class="featured" />
+            </header>
+            <div class="category">
+              @{{{_highlightResult.category.value}}}
+            </div>
+            <h2 class="entry-title">
+                @{{{_highlightResult.name.value}}}
+            </h2>
+
+            <div class="meta container">
+              <div class="row no-gutters">
+                <div class="col-5">
+                  <i class="fas fa-pencil-alt"></i>
+                  <span class="author">@{{{_highlightResult.author.value}}}</span>
+                </div>
+                <div class="col-5">
+                  <i class="fas fa-calendar-alt"></i>
+                  <time class="date">@{{date}}</time>
+                </div>
+                <div class="col-2">
+                  <i class="fas fa-comments"></i>
+                  <span
+                    class="comments disqus-comment-count"
+                    data-disqus-url="@{{permalink}}">
+                    0
+                  </span>
+                </div>
+              </div>
+            </div>
+          </a>
+        </article>
+      </script>
+      <!--
+      @while (have_posts()) @php(the_post())
+        <div class="col-md-3 col-sm-12 article-wrap">
+          @include('partials.content-'.get_post_type())
+        </div>
+      @endwhile
+      -->
+    </div>
+  </div>
+
+	<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/instantsearch.js@2.7.1/dist/instantsearch.min.css">
+	<script src="https://cdn.jsdelivr.net/npm/instantsearch.js@2.7.1"></script>
+
+	<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/instantsearch.js@2.7.1/dist/instantsearch-theme-algolia.min.css">
+
+  <script type="text/javascript">
+    jQuery(function() {
+      const search = instantsearch({
+        appId: algolia.application_id,
+        apiKey: algolia.search_api_key,
+        indexName: algolia.indices.posts_articol_stiri.name,
+        routing: true,
+        searchParameters: {
+          hitsPerPage: 200,
+          facetingAfterDistinct: true,
+          filters: 'locale:"' + current_locale + '"',
+        }
+      });
+
+      search.addWidget(
+        instantsearch.widgets.searchBox({
+          container: '#algolia-search-box',
+          placeholder: '{{ pll__("Cauta articole") }}',
+          cssClasses: {
+            input: 'form-control',
+          },
+          poweredBy: {
+            template: document.getElementById('powered-by-template').innerHTML,
+            cssClasses: 'powered-by',
+          }
+        })
+      );
+
+			search.addWidget(
+				instantsearch.widgets.hits({
+					container: '#algolia-hits',
+					templates: {
+						item: document.getElementById('hit-template').innerHTML,
+						empty: "{{ pll__('Nu am gasit rezultate pentru ')}} '@{{query}}'"
+					},
+					cssClasses: {
+						root: 'row no-gutters posts',
+						item: 'col-md-3 col-sm-12 article-wrap',
+					},
+					transformData: {
+						item: function(hit) {
+							return hit;
+						}
+					}
+				})
+			);
+
+      search.start();
+    });
+  </script>
+@endsection
