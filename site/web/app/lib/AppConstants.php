146835d880e5620333c5f15f0cabdfba88e1ab7c
Claudiu Ceia <claudiu.ceia+1@gmail.com>
2018-05-04 09:53:23 +0300

    feat(news) news article page

--- site/web/app/lib/AppConstants.php
+++ site/web/app/lib/AppConstants.php
@@ -7,7 +7,7 @@ final class AppConstants {
   const POST_TYPE_VERTICAL = 'verticala';
   const POST_TYPE_PROJECT_STATUS = 'status';
   const POST_TYPE_LINKS = 'link';
-  const POST_TYPE_ARTICOL_STIRI = 'articol_stiri';
+  const POST_TYPE_ARTICOL_STIRI = 'stire';
 
   const PROJECT_GALLERY_METABOX = 'project_gallery';
   const PROJECT_GALLERY_METABOX_LIST = 'cmb_field_project_gallery';
