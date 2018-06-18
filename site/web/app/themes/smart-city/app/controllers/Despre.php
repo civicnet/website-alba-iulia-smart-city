b7ec116804086374755687154d80f1c57e5d6e36
ClaudiuCeia <claudiu.ceia+1@gmail.com>
2018-06-18 12:57:30 +0300

    fix(about) load proper section on permalink

--- site/web/app/themes/smart-city/app/controllers/Despre.php
+++ site/web/app/themes/smart-city/app/controllers/Despre.php
@@ -13,21 +13,26 @@ class Despre extends Controller {
 
     $ret = array();
     foreach ($sections as $section) {
-			$ret[] = array(
+			$ret[$section->ID] = array(
         'titlu' => get_field('titlu', $section->ID),
         'titlu_scurt' => get_field('titlu_scurt', $section->ID),
 				'continut_1' => get_field('continut_1', $section->ID),
 				'continut_2' => get_field('continut_2', $section->ID),
         'titlu_scurt' => get_field('titlu_scurt', $section->ID),
         'permalink' => get_permalink($section->ID),
+        'id' => $section->ID
 			);
     }
 
     return $ret;
   }
 
+  public static function current(): array {
+    return self::sections()[get_the_ID()];
+  }
+
   public static function first(): array {
-    return self::sections()[0];
+    return array_values(self::sections())[0];
   }
 }
 
