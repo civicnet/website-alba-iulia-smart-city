58cae544d15f5b775f436ca83569283877c76c4c
ClaudiuCeia <claudiu.ceia+1@gmail.com>
2018-06-27 12:33:20 +0300

    fix(about) one accordion per section

--- site/web/app/themes/smart-city/resources/views/partials/components/despre-section.blade.php
+++ site/web/app/themes/smart-city/resources/views/partials/components/despre-section.blade.php
@@ -12,37 +12,37 @@
           <div class="entry-content">
             {!! $section['continut_1'] !!}
 
-            @foreach ($section['documente'] as $document)
             <div id="accordion">
               <div class="card">
-                <div class="card-header" id="heading-{{ $document['id'] }}">
-                  <h5 class="mb-0">
-                    <button
-                      class="btn btn-link"
-                      data-toggle="collapse"
-                      data-target="#collapseOne"
-                      aria-expanded="true"
-                      aria-controls="collapseOne">
-                      {{ $document['nume'] }}
-                      <i class="fa fa-chevron-down pull-right"></i>
-                    </button>
-                  </h5>
-                </div>
-
-                <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
-                  <div class="card-body">
-                    <h3>{!! $document['titlu'] !!}</h3>
-                    {!! $document['descriere'] !!}
-
-                    <div class="pdf-download">
-                      <i class="fas fa-file-pdf" style="margin-right: 4px; color: #f00"></i>
-                      <a href="{{ $document['url'] }}" target="_blank">{{ pll__('Descarcă') }} PDF</a>
+                @foreach ($section['documente'] as $document)
+                    <div class="card-header" id="heading-{{ $document['id'] }}">
+                      <h5 class="mb-0">
+                        <button
+                          class="btn btn-link"
+                          data-toggle="collapse"
+                          data-target="#collapse-{{ $document['id'] }}"
+                          aria-expanded="true"
+                          aria-controls="collapse-{{ $document['id'] }}">
+                          {{ $document['nume'] }}
+                          <i class="fa fa-chevron-down pull-right"></i>
+                        </button>
+                      </h5>
                     </div>
-                  </div>
-                </div>
+
+                    <div id="collapse-{{ $document['id'] }}" class="collapse show" aria-labelledby="heading-{{ $document['id'] }}" data-parent="#accordion">
+                      <div class="card-body">
+                        <h3>{!! $document['titlu'] !!}</h3>
+                        {!! $document['descriere'] !!}
+
+                        <div class="pdf-download">
+                          <i class="fas fa-file-pdf" style="margin-right: 4px; color: #f00"></i>
+                          <a href="{{ $document['url'] }}" target="_blank">{{ pll__('Descarcă') }} PDF</a>
+                        </div>
+                      </div>
+                    </div>
+                @endforeach
               </div>
             </div>
-            @endforeach
 
             {!! $section['continut_2'] !!}
           </div>
