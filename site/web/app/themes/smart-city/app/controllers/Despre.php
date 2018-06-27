007e0b33c320d4035b478b213dcf01a9a8985ee8
ClaudiuCeia <claudiu.ceia+1@gmail.com>
2018-06-27 10:03:08 +0300

    feat(about) allow attaching documents

--- site/web/app/themes/smart-city/app/controllers/Despre.php
+++ site/web/app/themes/smart-city/app/controllers/Despre.php
@@ -13,6 +13,21 @@ class Despre extends Controller {
 
     $ret = array();
     foreach ($sections as $section) {
+      $documents_raw = get_field('documente', $section->ID);
+      $documents_parsed = array();
+
+      if ($documents_raw) {
+        foreach($documents_raw as $doc) {
+          $documents_parsed[] = array(
+            'nume' => get_field('nume', $doc->ID),
+            'titlu' => get_field('titlu', $doc->ID),
+            'descriere' => get_field('descriere', $doc->ID),
+            'url' => get_field('document', $doc->ID),
+            'id' => $doc->ID,
+          );
+        }
+      }
+
 			$ret[$section->ID] = array(
         'titlu' => get_field('titlu', $section->ID),
         'titlu_scurt' => get_field('titlu_scurt', $section->ID),
@@ -20,6 +35,7 @@ class Despre extends Controller {
 				'continut_2' => get_field('continut_2', $section->ID),
         'titlu_scurt' => get_field('titlu_scurt', $section->ID),
         'permalink' => get_permalink($section->ID),
+        'documente' => $documents_parsed,
         'id' => $section->ID
 			);
     }
