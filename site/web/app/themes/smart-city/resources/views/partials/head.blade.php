86e05f82da8ce2f76bb67e9f7c8cb899576436d0
ClaudiuCeia <claudiu.ceia+1@gmail.com>
2018-07-05 17:59:32 +0300

    fix(head) add favicon

--- site/web/app/themes/smart-city/resources/views/partials/head.blade.php
+++ site/web/app/themes/smart-city/resources/views/partials/head.blade.php
@@ -2,6 +2,7 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="icon" type="image/png" href="@asset('images/favicon.png')" />
   <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
   <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:100,300,400,700" rel="stylesheet">
