0d594a0a30ab7815b07695d4ad12de85246ee664
ClaudiuCeia <claudiu.ceia+1@gmail.com>
2018-07-05 15:38:28 +0300

    fix(home) align about buttons

--- site/web/app/themes/smart-city/resources/views/partials/components/about-smart-city.blade.php
+++ site/web/app/themes/smart-city/resources/views/partials/components/about-smart-city.blade.php
@@ -1,28 +1,35 @@
 <div class="row about-section">
-    <div class="container">
-        <div class="row">
-            <div class="col-6 text-center">
-                <div class="icon">
-                    <img src="@asset('images/smart_city_icon.png')" />
-                </div>
-                <div class="cta oras-inteligent">
-                    {{ pll__('Ce este un oraș inteligent?') }}
-                </div>
-                <a class="button" href="{{ $ce_este }}">
-                    {{ pll__('Vezi detalii') }} >
-                </a>
-            </div>
-            <div class="col-6 text-center">
-                <div class="icon">
-                    <img src="@asset('images/poarta3_icon.png')" />
-                </div>
-                <div class="cta alba-iulia">
-                    {{ pll__('De ce Alba Iulia?') }}
-                </div>
-                <a class="button" href="{{ $de_ce }}">
-                    {{ pll__('Vezi detalii') }} >
-                </a>
-            </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-6 text-center">
+        <div class="icon">
+          <img src="@asset('images/smart_city_icon.png')" />
         </div>
+        <div class="cta oras-inteligent">
+          {{ pll__('Ce este un oraș inteligent?') }}
+        </div>
+      </div>
+      <div class="col-6 text-center">
+        <div class="icon">
+          <img src="@asset('images/poarta3_icon.png')" />
+        </div>
+        <div class="cta alba-iulia">
+          {{ pll__('De ce Alba Iulia?') }}
+        </div>
+      </div>
     </div>
+    <div class="row">
+      <div class="col-6 text-center">
+        <a class="button" href="{{ $ce_este }}">
+          {{ pll__('Vezi detalii') }} >
+        </a>
+      </div>
+      <div class="col-6 text-center">
+        <a class="button" href="{{ $de_ce }}">
+          {{ pll__('Vezi detalii') }} >
+        </a>
+      </div>
+    </div>
+  </div>
 </div>
