63b1fa34656fc3ffece5a5eafc81e0cf2d163f5b
ClaudiuCeia <claudiu.ceia+1@gmail.com>
2018-07-09 02:05:56 +0300

    fix(despre) sidebar to dropdown on mobile

--- site/web/app/themes/smart-city/resources/views/partials/components/despre-section.blade.php
+++ site/web/app/themes/smart-city/resources/views/partials/components/despre-section.blade.php
@@ -1,6 +1,25 @@
 <article class="type-despre">
   <div class="container-fluid article-container">
     <div class="container">
+      <div class="dropdown show small-menu">
+        <a
+          class="btn btn-secondary dropdown-toggle d-lg-none"
+          href="#"
+          role="button"
+          id="dropdownMenuLink"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false">
+          {{ pll__('Vezi alte sectiuni') }}
+        </a>
+
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+          @foreach ($all as $sec)
+            <a class="dropdown-item" href="{{ $sec['permalink'] }}">{{ $sec['titlu_scurt'] }}</a>
+          @endforeach
+        </div>
+      </div>
+
       <div class="row">
         <div class="col-md-9 col-sm-12">
           <h1 class="entry-title">{{ $section['titlu'] }}</h1>
@@ -49,7 +68,7 @@
           </div>
         </div>
 
-        <div class="col-md-4 col-sm-12 sidebar">
+        <div class="col-md-4 col-sm-12 sidebar d-none d-lg-block">
           <h3>{{ pll__('Categorii') }}</h3>
           <ul>
             @foreach ($all as $sec)
