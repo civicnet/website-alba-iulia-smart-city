654878d698e6c4cf38dd0fe942c49a5ba5fead0b
ClaudiuCeia <claudiu.ceia+1@gmail.com>
2018-07-09 01:15:13 +0300

    fix(homepage) adjust title height on featured news articles

--- site/web/app/themes/smart-city/resources/views/partials/components/article-box.blade.php
+++ site/web/app/themes/smart-city/resources/views/partials/components/article-box.blade.php
@@ -1,9 +1,9 @@
 <div class="col-lg-4 col-md-6 col-sm-12">
   <div class="article-box">
-    <h3>{{ $articol['title'] }}</h3>
+    <h3 style="min-height: 75px">{{ $articol['title'] }}</h3>
     <img src="{{ $articol['image'] }}" />
     <div class="excerpt">
-      {{ $articol['excerpt'] }} 
+      {{ $articol['excerpt'] }}
     </div>
     <a href="{{ $articol['permalink'] }}">
       {{ pll__('Citește') }} >
