d2d86b67dec7277376c5dc5aea6952246fb5e942
ClaudiuCeia <claudiu.ceia+1@gmail.com>
2018-07-14 16:54:34 +0300

    fix(home) responsive article box

--- site/web/app/themes/smart-city/resources/views/partials/components/article-box.blade.php
+++ site/web/app/themes/smart-city/resources/views/partials/components/article-box.blade.php
@@ -1,7 +1,9 @@
 <div class="col-lg-4 col-md-6 col-sm-12">
   <div class="article-box">
-    <h3 style="min-height: 75px">{{ $articol['title'] }}</h3>
-    <img src="{{ $articol['image'] }}" />
+    <h3>{{ $articol['title'] }}</h3>
+    <div class="article-img-wrapper">
+      <img src="{{ $articol['image'] }}" />
+    </div>
     <div class="excerpt">
       {{ $articol['excerpt'] }}
     </div>
