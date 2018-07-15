49d41008391d193f55105551946bad401245f5a4
ClaudiuCeia <claudiu.ceia+1@gmail.com>
2018-07-15 16:38:48 +0300

    fix(contact) stylize contact address, and add friendly links

--- site/web/app/themes/smart-city/resources/views/partials/contact-content-page.blade.php
+++ site/web/app/themes/smart-city/resources/views/partials/contact-content-page.blade.php
@@ -3,11 +3,34 @@
   <div class="row">
     <div class="col-md-6 col-sm-12">
         <div class="address">
-          {{ pll__('Adresă') }}: Calea Moţilor, 5A,  510134 Alba Iulia, România <br/>
-          {{ pll__('Telefon') }}: 0372 586 428 / 0258 819 462;<br/>
-          <br/>
-          {{ pll__('Fax') }}: 0258 812​ ​545<br/>
-          {{ pll__('E-mail') }}: {{ pll__('salut@albaiuliasmartcity.ro') }}<br/>
+          <div class="address-row">
+            <span><i class="fas fa-map-marker-alt"></i></span>
+            <span>
+              {{ pll__('Adresă') }}: 
+              <a
+                target="_blank" 
+                href="http://maps.google.com/?q=Calea Moţilor, 5A,  510134 Alba Iulia, România">
+                Calea Moţilor, 5A,  510134 Alba Iulia, România
+              </a>
+            </span> 
+          </div>
+          <div class="address-row">
+            <span><i class="fas fa-phone"></i></span>
+            <span>
+              {{ pll__('Telefon') }}: 
+              <a href="tel:0372 586 428" class="no-break">0372 586 428</a> / 
+              <a href="tel:0258 819 462" class="no-break">0258 819 462</a>
+            </span>
+          </div>
+          <div class="address-row">
+            <span><i class="fas fa-envelope"></i></span>
+            <span>
+              {{ pll__('E-mail') }}: 
+              <a href="mailto:{{ pll__('salut@albaiuliasmartcity.ro') }}" class="no-break">
+                {{ pll__('salut@albaiuliasmartcity.ro') }}
+              </a>
+            </span>
+          </div>
         </div>
     </div>
     <div class="col-md-6 col-sm-12">
