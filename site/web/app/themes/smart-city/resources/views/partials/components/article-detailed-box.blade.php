a06ddac56c036edae04210b6cb2511625ce62ec9
ClaudiuCeia <claudiu.ceia+1@gmail.com>
2018-07-16 03:08:54 +0300

    fix(articles) fix related metadata alignment

--- site/web/app/themes/smart-city/resources/views/partials/components/article-detailed-box.blade.php
+++ site/web/app/themes/smart-city/resources/views/partials/components/article-detailed-box.blade.php
@@ -10,26 +10,24 @@
 			{{ $article['title'] }}
 		</h2>
 
-		<div class="meta container">
-			<div class="row no-gutters">
-				<div class="col-5">
-					<i class="fas fa-pencil-alt"></i>
-					<span class="author">{{ $article['author'] }}</span>
-				</div>
-				<div class="col-5">
-					<i class="fas fa-calendar-alt"></i>
-					<time class="date">{{ date('d M Y', strtotime($article['date'])) }}</time>
-				</div>
-				<div class="col-2">
-					<i class="fas fa-comments"></i>
-					<span
-						class="comments disqus-comment-count"
-						data-disqus-url="{{ $article['permalink'] }}">
-						0
-					</span>
-				</div>
-			</div>
-		</div>
+        <div class="meta container">
+          <div>
+            <i class="fas fa-pencil-alt"></i>
+            <span class="author">{{ $article['author'] }}</span>
+          </div>
+          <div> 
+            <i class="fas fa-calendar-alt"></i>
+            <time class="date">{{ date('d M Y', strtotime($article['date'])) }}</time>
+          </div>
+          <div>
+            <i class="fas fa-comments"></i>
+            <span
+              class="comments disqus-comment-count"
+              data-disqus-url="{{ $article['permalink'] }}">
+              0
+            </span>
+          </div>
+        </div>
 	</a>
 </article>
 
