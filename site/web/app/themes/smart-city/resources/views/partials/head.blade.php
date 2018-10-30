95e499599e3b12f50d5fcebbfe52f811f56acb68
ClaudiuCeia <claudiu.ceia+1@gmail.com>
2018-10-30 15:26:54 +0200

    fix(clym) add domain confirmation meta tag

--- site/web/app/themes/smart-city/resources/views/partials/head.blade.php
+++ site/web/app/themes/smart-city/resources/views/partials/head.blade.php
@@ -2,6 +2,7 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="clym-site-verification" content="clymk0yvdr7meeuovrgeef7oe90q"/>
   <link rel="icon" type="image/png" href="@asset('images/favicon.png')" />
   <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
   <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300|Roboto:300,400,500,700|Roboto+Slab:100,300,400,700" rel="stylesheet">
