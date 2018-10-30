a74db8f75fba98bbb171dd92f076d9a3ffedbc31
ClaudiuCeia <claudiu.ceia+1@gmail.com>
2018-10-30 15:45:00 +0200

    fix(clym) add clym widget

--- site/web/app/themes/smart-city/resources/views/partials/head.blade.php
+++ site/web/app/themes/smart-city/resources/views/partials/head.blade.php
@@ -7,5 +7,16 @@
   <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
   <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300|Roboto:300,400,500,700|Roboto+Slab:100,300,400,700" rel="stylesheet">
 
+  <script>
+    (function(d,s,i,w,o){
+    var js,cjs=d.getElementsByTagName(s)[0];
+    if(d.getElementById(i))return;
+    js=d.createElement('script');
+    js.id=i;
+    js.src="https://widget.clym-sdk.net/clym.js";
+    js.onload=function(){Clym&&Clym.load(i,w,o);};
+    cjs.parentNode.insertBefore(js, cjs);
+    }(document,'script','clym-privacy','909216522c4b4e65811fba8ch8lrnY9I'));
+  </script>
   @php(wp_head())
 </head>
